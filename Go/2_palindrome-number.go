805ecae25304da645845d908dd53ab087a82bc78
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2025-02-16 14:12:15 +0900

    別解を追加

--- Go/2_palindrome-number.go
+++ Go/2_palindrome-number.go
@@ -48,3 +48,18 @@ func isPalindrome(x int) bool {
 
 	return true
 }
+
+// best answer? ↓
+func isPalindrome2(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	rev := 0
+	for x > rev {
+		rev = rev*10 + x%10
+		x /= 10
+	}
+
+	return x == rev || x == rev/10
+}
